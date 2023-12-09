d8494f868939f05d937bb932dea00849d64555c8
aikanyshulanbekova <ulanbebekovaajkanys5@gmail.com>
2023-12-09 17:23:40 +0300

    work

--- z2 .py
+++ z2 .py
@@ -3,6 +3,24 @@
 
 import random
 
+class Matrix:
+    def __init__(self, n, m, a, b):
+        """
+        n - Ширина матрицы
+        m - Высота матрицы
+        a и b - Диапазоны случайных чисел для матрицы
+        """
+        self._matrix = []
+
+        for i in range(0, n):
+            r = []
+            for j in range(0, m):
+                r.append(random.randint(a, b))
+            self._matrix.append(r)
+
+    def __str__(self):
+        return str(self._matrix)
+
 if __name__ == '__main__':
     try:
         n = int(input("Введите высоту матрицы: "))
@@ -13,12 +31,6 @@ if __name__ == '__main__':
         print("Ошибка: Необходимо ввести числа")
         exit(1)
 
-    matrix = []
-
-    for i in range(0, n):
-        r = []
-        for j in range(0, m):
-            r.append(random.randint(a, b))
-        matrix.append(r)
+    matrix = Matrix(n, m, a, b)
 
     print(matrix)
